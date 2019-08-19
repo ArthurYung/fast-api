f8989a9845b02969f10d24cfa803b57aad32784a
v_ycyou <v_ycyou@tencent.com>
2019-08-19 16:37:38 +0800

    Updates

---
+++ precache-manifest.7adc954343767c34cbe4a00ceaf51ecb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df02e87ff785acae22aad2d982fa4ca",
+    "url": "/fast-api/index.html"
+  },
+  {
+    "revision": "21fbca764ba6ed86da66",
+    "url": "/fast-api/static/css/main.92b2c2f2.chunk.css"
+  },
+  {
+    "revision": "0b841545c754fd3f83b5",
+    "url": "/fast-api/static/js/2.fa432fd8.chunk.js"
+  },
+  {
+    "revision": "1a11449690963bb31da1",
+    "url": "/fast-api/static/js/3.fcc1d647.chunk.js"
+  },
+  {
+    "revision": "99391b0f543793ca7bd8",
+    "url": "/fast-api/static/js/4.2e0f3687.chunk.js"
+  },
+  {
+    "revision": "d8e95ad643fc761a5304",
+    "url": "/fast-api/static/js/5.edf59a78.chunk.js"
+  },
+  {
+    "revision": "21fbca764ba6ed86da66",
+    "url": "/fast-api/static/js/main.216dfbf3.chunk.js"
+  },
+  {
+    "revision": "cdf25ae6dfb73cb8fcc8",
+    "url": "/fast-api/static/js/runtime~main.f5f36fba.js"
+  }
+]);
