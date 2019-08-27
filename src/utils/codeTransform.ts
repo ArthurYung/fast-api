073f5b89e1dcfa0227fb14751e4065bb5135d210
v_ycyou <v_ycyou@tencent.com>
2019-08-27 20:57:46 +0800

    feat: id log

--- src/utils/codeTransform.ts
+++ src/utils/codeTransform.ts
@@ -1,27 +1,50 @@
 import { transformFromAstSync } from "@babel/core";
 import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
+import traverse, { NodePath } from "@babel/traverse";
 import * as t from "@babel/types";
-import { isEmptyFunction } from "./transformCommon";
+import { isEmptyFunction, findFunctionName, addId } from "./transformCommon";
 
 let _gid = 0;
-
 function getGid() {
   return ++_gid + "";
 }
 
-function parseToAst(code: string) {
-  const _gid = getGid();
+function _parseToAst(code: string) {
+  const gid = getGid();
+  const funcNameMap: { [x: string]: string } = {};
   const ast = parse(code, { sourceType: "module" });
   traverse(ast, {
     Function(path) {
       if (isEmptyFunction(path)) return;
-      console.log(path);
-      const _tid = path.scope.generateUidIdentifier("tid");
-      console.log(_tid);
+      const uid = path.scope.generateUidIdentifier("_uid");
+      const tid = path.scope.generateUid("_tid");
+      const name = findFunctionName(path);
+      const query: {
+        gid: string;
+        tid: string;
+        uid: t.Identifier;
+        path: NodePath<t.Function>;
+      } = { gid, uid, tid, path };
+      console.log(addId(query));
+      // console.log(
+      //   path
+      //     .get("body")
+      //     .get("body")
+      //     .unshift(addId(query))
+      // );
+      (path.get("body") as any).unshiftContainer("body", addId(query));
+      // console.log(path);
+      funcNameMap[tid] = name;
+      console.log();
     }
   });
   return ast;
+}
+
+function parseToAst(_code: string) {
+  const ast = _parseToAst(_code);
+  let reslt = transformFromAstSync(ast);
+  console.log(reslt);
 }
 
 export { parseToAst };
--- src/utils/transformCommon.ts
+++ src/utils/transformCommon.ts
@@ -9,4 +9,40 @@ function isEmptyFunction(path: NodePath<t.Function>): boolean {
   return false;
 }
 
-export { isEmptyFunction };
+function findFunctionName(path: any): string {
+  const parentNode = path.parentPath.node;
+  let functionName = "";
+
+  if (parentNode.type === "AssignmentExpression") {
+    functionName = parentNode.left.property.name;
+  } else if (parentNode.type === "VariableDeclarator") {
+    functionName = parentNode.id.name;
+  } else if (parentNode.type === "CallExpression") {
+    functionName = parentNode.callee.name + "<CallBack>";
+  } else {
+    functionName = path.node.id ? path.node.id.name : "anonymous";
+  }
+
+  return functionName;
+}
+
+function addId(query: {
+  gid: string;
+  tid: string;
+  uid: t.Identifier;
+  path: NodePath<t.Function>;
+}) {
+  console.log(query);
+  var result = t.variableDeclaration("var", [
+    t.variableDeclarator(
+      query.uid,
+      t.callExpression(
+        t.memberExpression(t.identifier("time"), t.identifier("start")),
+        [t.identifier("1")]
+      )
+    )
+  ]);
+  return result;
+}
+
+export { isEmptyFunction, findFunctionName, addId };
